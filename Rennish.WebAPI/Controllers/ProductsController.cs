e840cd0ee58563334d1d66f4250f823b59255f19
rennishj <rennishj@gmail.com>
2014-03-08 21:09:49 -0500

    Added more Api controllers

---
+++ Rennish.WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Rennish.WebAPI.Models;
+using Rennish.WebAPI.Repository;
+
+namespace Rennish.WebAPI.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private static IProductRepository _repo = new ProductRepository();
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _repo.GetAll();
+        }
+
+        public Product GetProductById(int Id)
+        {
+            var product = _repo.Get(Id);
+            if (product == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            else
+            {
+                return product;
+            }
+        }
+
+        [HttpPost]
+        public HttpResponseMessage CreateProduct(Product item)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = _repo.Add(item);
+                var response = Request.CreateResponse<Product>(HttpStatusCode.Created, result);
+                var uri = Url.Link("DefaultApi", new { id = result.Id });
+                response.Headers.Location = new Uri(uri);
+                return response;
+            }
+
+            return new HttpResponseMessage(HttpStatusCode.BadRequest);
+        }
+    }
+}
---
+++ Rennish.WebAPI/Repository/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rennish.WebAPI.Models;
+
+namespace Rennish.WebAPI.Repository
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetAll();
+        Product Get(int id);
+        Product Add(Product item);
+        void Remove(int id);
+        bool Update(Product item);
+    }
+
+}
